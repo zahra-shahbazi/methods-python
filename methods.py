c6ceab0f4da25473651cb369a8117b108599612c
zahra shahbazi <zahra.shahbazi.nafas@gmail.com>
2025-11-29 21:06:44 +0330

    learnin methods

---
+++ methods.py
@@ -0,0 +1,17 @@
+text='Hello word my name is zahra shahbazi'
+
+print(text.upper())      #تبدیل به حروف بزرگ 
+
+
+print(text.capitalize())    #بزرگ کردن حرف اول
+
+
+print(text.find('n'))       # مشخص کردن جایگاه حروف تعیین شده
+
+print(text.count("a"))       # مشخص کرددن حروف تعیین شده که چند بار داخل جمه تکرار شده
+
+print(text.casefold())       # کوچک کردن حروف بزرگ
+
+print(text.lower())           # تبدیلل به حروف کوچک
+
+print(text.title())          # بزرگ کرددن حروف اول هر کلمه
